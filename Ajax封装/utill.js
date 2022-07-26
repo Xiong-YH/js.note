cc2046463099d9ddb466043bf7318a26f1863423
Xiong-YH <974205071@qq.com>
2022-07-26 21:10:34 +0800

    Update utill.js

--- Ajax封装/utill.js
+++ Ajax封装/utill.js
@@ -1,1 +1,28 @@
 //工具函数
+const serialize = param => {
+    //对象变为字符串 == 对象->数组->字符串
+    const result = [];
+
+    for (const [key, value] of Object.entries.param) {
+        result.push(`${encodeURIComponent(key)} = ${encodeURIComponent(value)}`)
+    };
+
+    return result.join('&');
+};
+
+
+//给URL添加参数
+
+const addURLData = (url,data) => {
+    if(!data) return '';
+
+
+    const mark = url.includes('?')?'&':'?';
+
+
+    return `${mark}${data}`;
+
+}
+
+
+export {serialize,addURLData};
