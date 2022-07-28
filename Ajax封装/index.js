78d68bdcab9952b6d6f8cb7673232d5c624038e8
Xiong-YH <974205071@qq.com>
2022-07-28 19:39:39 +0800

    Update index.js

--- Ajax封装/index.js
+++ Ajax封装/index.js
@@ -1,7 +1,57 @@
 import Ajax from "./Ajax.js";
 
+
+//常量
+import { ERROR_ABORT, ERROR_HTTP_CODE, ERROR_REQUEST, ERROR_TIMEOUT,
+        ERROR_ABORT_TEXT,ERROR_HTTP_CODE_TEXT,ERROR_REQUEST_TEXT,ERROR_TIMEOUT_TEXT } from "./constents.js";
+
 const ajax = (url, options) => {
-    return new Ajax(url, options).getXHR();
+    // return new Ajax(url, options).getXHR();
+    let xhr;
+    const p = new Promise((resolve, reject) => {
+        xhr = new Ajax(url, {
+            ...options,
+            ...{
+                success(response) {
+                    resolve(response);
+                },
+                httpCodeError(status) {
+                    reject({
+                        type: ERROR_HTTP_CODE,
+                        text: `${ERROR_HTTP_CODE_TEXT}:${status}`,
+                    })
+                },
+                error(){
+                    reject({
+                        type:ERROR_REQUEST,
+                        text: ERROR_REQUEST_TEXT,
+                    })
+                },
+                abort(){
+                    reject({
+                        type:ERROR_ABORT,
+                        text: ERROR_ABORT_TEXT,
+                    })
+                },
+                timeoutr(){
+                    reject({
+                        type:ERROR_TIMEOUT,
+                        text: ERROR_TIMEOUT_TEXT,
+                    })
+                },
+            },
+        }).getXHR();
+    });
+
+
+    p.xhr = xhr;
+
+    p.ERROR_ABORT = ERROR_ABORT;
+    p.ERROR_HTTP_CODE = ERROR_HTTP_CODE;
+    p.ERROR_REQUEST = ERROR_REQUEST;
+    p.ERROR_TIMEOUT = ERROR_TIMEOUT;
+
+    return p;
 };
 
 const get = (url, options) => {
